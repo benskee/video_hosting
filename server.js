666bb185fbf821d3bb90bd09a94f2fb8d3ceecf6
benskee <benskee33@gmail.com>
2022-01-06 14:06:49 -0500

    sever update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
 const app = express();
 var cors = require('cors');
 app.use(cors())
@@ -11,6 +12,15 @@ app.use(express.json({ extended: false }))
 app.use('/api/file', require('./api/file'));
 app.use('/api/users', require('./api/users'));
 app.use('/api/auth', require('./api/auth'));
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
